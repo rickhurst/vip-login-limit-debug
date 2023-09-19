aba00a08805f4692a2597995e1d79d32b469108e
Rick Hurst <rick.hurst@automattic.com>
2023-09-19 17:47:10 +0100

    updated to reflect recent changes in mu plugins and to provide sample wp cli commands for removing locks

--- vip-login-limit-debug.php
+++ vip-login-limit-debug.php
@@ -3,43 +3,105 @@
 Plugin Name: VIP login debug
 Plugin URI:  https://wordpressvip.com
 Description: Debug info for locked out logins
-Version:     1.0
+Version:     1.1
 Author:      Rick Hurst
 Author URI:  https://wordpressvip.com
 */
+
 
 /* Add message above login form */
 function vip_debug_login_message() {
 
     // store some key/values which might be useful for debugging login limit
-    $login_vars = [];
+    $login_debug_vars = [];
+
+    $login_debug_vars['cache_key_lock_prefix'] = CACHE_KEY_LOCK_PREFIX;
+    $login_debug_vars['cache_group_login_limit'] = CACHE_GROUP_LOGIN_LIMIT;
+    $login_debug_vars['cache_group_lost_password_limit'] = CACHE_GROUP_LOST_PASSWORD_LIMIT;
+    $login_debug_vars['cache_key_lock_prefix'] = CACHE_KEY_LOCK_PREFIX;
 
     // IP address
-    $login_vars['ip']   = preg_replace( '/[^0-9a-fA-F:., ]/', '', $_SERVER['REMOTE_ADDR'] ); //phpcs:ignore
+    if( isset($_GET['ip']) && "" !== $_GET['ip'] ){ //phpcs:ignore
+        $login_debug_vars['ip'] = $_GET['ip'];
+    } else {
+        $login_debug_vars['ip']   = preg_replace( '/[^0-9a-fA-F:., ]/', '', $_SERVER['REMOTE_ADDR'] ); //phpcs:ignore
+    }
+
+    $thresholds = _vip_get_auth_count_limit_defaults();
+    $login_debug_vars = array_merge( $login_debug_vars, $thresholds );
 
     // check if the username is set as limited
     if( isset($_GET['username']) && "" !== $_GET['username'] ){ //phpcs:ignore
-        $username = $_GET['username']; //phpcs:ignore
-        $login_vars['ip_username_cache_key'] = $login_vars['ip'] . '|' . $username;
-        $login_vars['ip_count'] = wp_cache_get( $login_vars['ip'], 'login_limit' );
-        $login_vars['ip_username_count'] = wp_cache_get( $login_vars['ip_username_cache_key'], 'login_limit' );
-        $login_vars['username_count'] = wp_cache_get( $username, 'login_limit' );
+        $raw_username = $_GET['username']; //phpcs:ignore
+        $cache_keys = vip_login_debug_get_cache_keys( $raw_username, $login_debug_vars['ip'] );
+        $login_debug_vars = array_merge( $login_debug_vars, $cache_keys );
 
-        $ip_username_threshold = apply_filters( 'wpcom_vip_ip_username_login_threshold', 5, $login_vars['ip'], $username );
-        $ip_threshold = apply_filters( 'wpcom_vip_ip_login_threshold', 50, $login_vars['ip'] );
-        $username_threshold = 5 * $ip_username_threshold; // Default to 5 times the IP + username threshold
-        $username_threshold = apply_filters( 'wpcom_vip_username_login_threshold', $username_threshold, $username );
-
-        $login_vars['ip_username_threshold'] = $ip_username_threshold;
-        $login_vars['ip_threshold'] = $ip_threshold;
-        $login_vars['username_threshold'] = $username_threshold;
+        $cache_group = CACHE_GROUP_LOGIN_LIMIT;
+        $login_debug_vars['is_ip_username_locked'] = wp_cache_get( CACHE_KEY_LOCK_PREFIX . $cache_keys['ip_username_cache_key'], $cache_group );
+        $login_debug_vars['is_ip_locked'] = wp_cache_get( CACHE_KEY_LOCK_PREFIX . $cache_keys['ip_cache_key'], $cache_group );
+        $login_debug_vars['is_username_locked'] = wp_cache_get( CACHE_KEY_LOCK_PREFIX . $cache_keys['username_cache_key'], $cache_group );
     }
 
-    $debug_info = var_export($login_vars, true); //phpcs:ignore
+    $output = '<div>';
+    $output .= vip_login_debug_display_array_as_table($login_debug_vars);
+    $output .= vip_login_debug_cache_commands($cache_keys);
+    $output .= '</div>';
 
-    return '<p class="message"><b>VIP debug info</b><br>'.$debug_info.'</p>'; //phpcs:ignore
-
+    return $output; //phpcs:ignore
 }
+
+/**
+ * Output an associative array as an HTML table with WordPress escaping.
+ *
+ * @param array $data The associative array to be displayed as a table.
+ */
+function vip_login_debug_display_array_as_table($data) {
+    if (empty($data) || !is_array($data)) {
+        return '';
+    }
+
+    $table_html = '<table style="background:white;margin-bottom: 20px;">';
+
+    foreach ($data as $key=>$val) {
+
+        if(is_bool($val)){
+            $val = (true === $val) ? 'yes' : 'no';
+        }   
+
+        $table_html .= '<tr>';
+        $table_html .= '<th style="text-align:left;padding:3px;">' . esc_html($key) . '</th>';
+        $table_html .= '<td style="text-align:left;padding:3px;">' . esc_html($val) . '</td>';
+        $table_html .= '</tr>';
+    }
+
+    $table_html .= '</table>';
+
+    return $table_html;
+}
+
+function vip_login_debug_cache_commands( $cache_keys ){
+    $output = '<div>';
+    $output .= '<pre>';
+    foreach($cache_keys as $key){
+        $output .= 'wp cache delete "' . CACHE_KEY_LOCK_PREFIX . $key . '" ' . CACHE_GROUP_LOGIN_LIMIT . PHP_EOL;
+    }
+    $output .= '</pre></div>';
+    return $output;
+}
+
+function vip_login_debug_get_cache_keys( $raw_username, $ip ){
+            $username = vip_strict_sanitize_username( $raw_username );
+        
+            // phpcs:ignore WordPressVIPMinimum.Variables.ServerVariables.UserControlledHeaders
+            $ip = filter_var( $ip ?? '', FILTER_VALIDATE_IP, [ 'options' => [ 'default' => '' ] ] );
+        
+            return [
+                'ip_username_cache_key' => $ip . '|' . $username,
+                'ip_cache_key'          => $ip,
+                'username_cache_key'    => $username,
+            ];
+}
+
 
 if( isset($_GET['vip_login_debug']) ){ //phpcs:ignore
     add_filter('login_message', 'vip_debug_login_message');
